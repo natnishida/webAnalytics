331b1449f22e16c6946972314297b6344cabfce6
Natasha Nishida <42380371+natnishida@users.noreply.github.com>
2019-07-10 14:52:30 -0300

    Update index.php

--- CodigoAnalytics/index.php
+++ CodigoAnalytics/index.php
@@ -1,16 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-143612291-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-143612291-1');
-</script>
-
     <meta charset="utf-8">
     <!-- Responsive -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
